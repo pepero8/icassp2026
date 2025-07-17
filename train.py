d410cf936788dc965645995ff3289a6c8d7a3292
jaehwan <jhwan98@postech.ac.kr>
2025-07-17 10:27:32 +0000

    Change patience

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from lightning.pytorch.loggers import TensorBoardLogger
 
 app = typer.Typer(pretty_exceptions_enable=False)
 
+
 @app.command()
 def main(
     cfg: Annotated[str, typer.Argument(help="Path to the config file")],
@@ -29,7 +30,7 @@ def main(
     early_stop_callback = EarlyStopping(
         monitor="val_loss",
         min_delta=0.00,
-        patience=3,
+        patience=7,
         verbose=False,
         mode="min",
         check_finite=True,
@@ -46,12 +47,19 @@ def main(
     #     callbacks=[early_stop_callback],
     #     strategy=ddp_strategy,
     # ) # ? for testing
-    
+
     # > Generate version based on current timestamp
     current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     # > Create a logger instance
-    logger = TensorBoardLogger(config.default_root_dir, version=current_time, name="icassp2026")
-    checkpoint_callback = ModelCheckpoint(save_top_k=5, monitor="val_loss", mode="min", filename="epoch{epoch:02d}-val_loss:{val_loss:.4f}") # 기본적으로 Trainer.log_dir에 저장됨
+    logger = TensorBoardLogger(
+        config.default_root_dir, version=current_time, name="icassp2026"
+    )
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=5,
+        monitor="val_loss",
+        mode="min",
+        filename="epoch{epoch:02d}-val_loss:{val_loss:.4f}",
+    )  # 기본적으로 Trainer.log_dir에 저장됨
 
     trainer = L.Trainer(
         num_sanity_val_steps=1,
@@ -75,4 +83,4 @@ if __name__ == "__main__":
     app()
     # typer.run(main)
 
-# CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=1 python train.py config.yaml
+# CUDA_VISIBLE_DEVICES=3 python train.py config.yaml
